ee7dfadf3f7bbb0a3d61c2978609497b5f5fc9b5
Radhakrishnan C <radhakrish2@gmail.com>
2025-03-23 22:55:00 +0530

    commit 7d

--- src/main/java/com/demo/EmployeeService/EmployeeController.java
+++ src/main/java/com/demo/EmployeeService/EmployeeController.java
@@ -11,7 +11,7 @@ public class EmployeeController {
 	@GetMapping
 	public String getMessage(String[] args)
 	{
-		return "welcome to spring boot  rk ff";
+		return "welcome to spring boot beh";
 	}
 	
 }
