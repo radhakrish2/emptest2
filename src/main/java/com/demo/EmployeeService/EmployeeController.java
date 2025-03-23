37adc2d4b5b6926acbab3898a8547e73ee1a4ee0
Radhakrishnan C <radhakrish2@gmail.com>
2025-03-23 18:31:50 +0530

    commit1

---
+++ src/main/java/com/demo/EmployeeService/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.demo.EmployeeService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("employee")
+public class EmployeeController {
+
+	@GetMapping
+	public String getMessage(String[] args)
+	{
+		return "welcome to spring";
+	}
+	
+}
